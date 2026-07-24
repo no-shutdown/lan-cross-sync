9e1bf2c9e5ab66d74d9291adb98102cc37f44cfd
xinlin.huang <xinlin.huang@gitlab.bt.com>
2026-07-24 17:38:57 +0800

    feat: route drop-overlay window to DropOverlay component

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { getCurrentWebviewWindow } from "@tauri-apps/api/webviewWindow";
 import App from "./App";
+import { DropOverlay } from "./App";
+
+const _windowLabel = getCurrentWebviewWindow().label;
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    {_windowLabel === "drop-overlay" ? <DropOverlay /> : <App />}
   </React.StrictMode>,
 );
